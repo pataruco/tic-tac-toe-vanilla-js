ab9bd1230c6fa5c605fa5b24ef9e0b1ef75d56ad
Pedro Martin <pataruco@gmail.com>
2020-09-30 21:08:58 +0100

    Refactor js

--- scripts/index.js
+++ scripts/index.js
@@ -29,60 +29,30 @@ const checkIfPlayerWin = (player) =>
     solution.every((positionValue) => player === board[positionValue]),
   );
 
-// 4.1
 const addScore = (player) => {
-  // Evalua el jugador de turno ganador y añade mas uno al score anterior
-  if (player === 'x') {
-    playerXScore = playerXScore + 1;
-  } else {
-    playerOScore = playerOScore + 1;
-  }
-
-  // Imprime en pantalla el score nuevo cuando hay ganador
+  player === 'x' ? playerXScore++ : playerOScore++;
   playerXScoreElement.innerText = playerXScore;
   playerOScoreElement.innerText = playerOScore;
 };
 
-// 3. a cada cell que es cliqueada se corre lo siguiente
-
 const handleClick = (event) => {
-  // 3.1 salvo en una constante de ambito local (scope) el valor del elemento HTML cliqueado
   const cellClicked = event.target;
-  // 3.2 Inyecto como texto el jugador que le toca jugar
   cellClicked.innerText = playerTurn;
-  // 3.3 Remuevo el event listener para evitar que un cell cambie de valor una vez clicado
   cellClicked.removeEventListener('click', handleClick);
-  // 3.4 Identifico que posición dentro del tablero fue clickeada
   const positionPlayed = cells.indexOf(cellClicked);
-  // 3.5 Agrego en en el array tablero (board) en la posición identificada en el el paso 3.4
-  // el valor del jugador de turno (player)
   board[positionPlayed] = playerTurn;
-
-  // 3.6 Por cada click contrastar el array board (que tiene las posiciones jugadas por cada judador)
-  // versus el array de arrayes de soluciones (solutions), si un jugador (X o O) es igual en las las posiciones
-  // identificadas en uno de los arrayes de solucion declaro ganador
-
   const ifPlayerWin = checkIfPlayerWin(playerTurn);
 
-  // Mostrar ganador en pantalla
   if (ifPlayerWin) {
-    // 4. Suma + 1 al jugador ganador
     addScore(playerTurn);
   }
-  // Mostrar score de los jugadores
-
-  // 3.7 Cambio de jugador
   changePlayerToPlay();
 };
 
-// 2. por cada elemento dentro de section (lo llamamos cell) agrego un EventListener de Click
-// para que cada vez que haya click en ese cell se llame a la funcion handleClick
 const addHandleClick = (cell) => {
   cell.addEventListener('click', handleClick);
 };
 
-// 1. iterando por cada uno de los div que estan dentro de section en HTML (linea 13 de HTML)
-// y por cada uno corriendo la función addHandleClick
 cells.forEach(addHandleClick);
 
 // TODO:
